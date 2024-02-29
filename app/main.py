9293943939bc13d0f6134f57716d0c991181c9a3
frameworksselenium <frameworksselenium@gmail.com>
2024-02-28 23:21:41 -0500

    initial

--- app/main.py
+++ app/main.py
@@ -1,4 +1,13 @@
+import logging
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level to INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    print("Hello ECS Task")
+    logger.info("Hello ECS Task")
