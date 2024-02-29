8d6a07fa23a2427b066ed50684f47d140d0920b3
frameworksselenium <frameworksselenium@gmail.com>
2024-02-28 23:11:13 -0500

    initial

--- app/main.py
+++ app/main.py
@@ -1,14 +1,4 @@
-from fastapi import FastAPI
-
-app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    print("Hello ECS Task")
